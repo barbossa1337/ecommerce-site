08fd334120af595b176f3cafd63a4a175f2a3fdd
barbossa1337 <berkbuyukbarbaros@gmail.com>
2022-12-18 08:04:56 +0300

    ProductSlider done.

---
+++ src/Components/ProductDetail/ProductDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductSlider from "../Slider/ProductSlider";
+
+const ProductDetail = () => {
+    return (
+        <div>
+            <h1>ProductDetail</h1>
+            <ProductSlider />
+        </div>
+    )
+}
+
+export default ProductDetail;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductList from "../../Components/ProductList/ProductList";
+import ProductSlider from "../../Components/Slider/ProductSlider";
 
 const Home = () => {
     return (
     <div>Home Component
         <h2>Slider</h2>
         <h2>Category</h2>
+        <ProductSlider />
         <ProductList />
     </div>
     );
